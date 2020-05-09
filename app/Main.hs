5e329fcc9fe908c0e6e896df2779a6ef95a666be
Harry Askham <harryaskham@gmail.com>
2020-05-09 14:21:28 +0100

    Source cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,86 +1,10 @@
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiWayIf #-}
 
 module Main where
 
 import Control.Concurrent.MVar
-import System.Random
-import System.Random.Shuffle
-import Safe
 import Web.Firefly
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as TE
-import Network.HTTP.Types.Status
-import qualified Data.Map.Strict as M
-import Control.Monad.IO.Class
-import Data.Char
-import Control.Lens
-import Text.Blaze.Html
-import qualified Data.ByteString as B
-
-type Entry = T.Text
-
-data GameState = GameState { _entries :: MVar [Entry]
-                           , _unseen :: MVar [Entry]
-                           }
-makeLenses ''GameState
-
-cors :: ToResponse b => b -> (b, Status, HeaderMap)
-cors b = (b, status200, M.fromList [("Access-Control-Allow-Origin", ["*"])])
-
-addThingHandler :: GameState -> Handler (T.Text, Status, HeaderMap)
-addThingHandler st = do
-  name <- getQuery "name"
-  case name of
-       Just n ->
-         if all isSpace (T.unpack n)
-            then do
-              liftIO $ print "empty thing"
-              return $ cors "fail"
-            else do
-              liftIO $ print "Adding an entry"
-              liftIO $ modifyMVar_ (st^.entries) (pure . (n:))
-              return $ cors "added"
-       Nothing -> do
-         liftIO $ print "No name"
-         return $ cors "failure"
-
-resetHandler :: GameState -> Handler (T.Text, Status, HeaderMap)
-resetHandler st = do
-  g <- liftIO newStdGen
-  liftIO $ print "resetting the round"
-  es <- liftIO $ readMVar (st^.entries)
-  liftIO $ modifyMVar_ (st^.unseen) (const . return $ shuffle' es (length es) g)
-  return $ cors "reset"
-
-takeHandler :: GameState -> Handler (T.Text, Status, HeaderMap)
-takeHandler st = do
-  es <- liftIO $ readMVar (st^.unseen)
-  case headMay es of
-    Just e -> do
-      liftIO $ print "taking one thing"
-      liftIO $ modifyMVar_ (st^.unseen) (const . return $ tailSafe es)
-      return $ cors e
-    Nothing -> do
-      liftIO $ print "nothing left"
-      return $ cors "empty"
-
-putBackHandler :: GameState -> Handler (T.Text, Status, HeaderMap)
-putBackHandler st = do
-  name <- getQuery "name"
-  case name of
-    Just n -> do
-      g <- liftIO newStdGen
-      liftIO $ print "putting back"
-      us <- liftIO $ readMVar (st^.unseen)
-      let newUs = n:us
-      liftIO $ modifyMVar_ (st^.unseen) (const . return $ shuffle' newUs (length newUs) g)
-      return $ cors "put back"
-    Nothing -> do
-      liftIO $ print "invalid putback"
-      return $ cors "invalid"
+import Lib
 
 main :: IO ()
 main = do
